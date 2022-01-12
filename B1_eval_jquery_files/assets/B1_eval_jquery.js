58eaf89498fa4d8f5452c5aa0c68023a90f325fb
Jeanne04 <jane22jene04@icloud.com>
2022-01-12 09:37:00 +0100

    question1

--- B1_eval_jquery_files/assets/B1_eval_jquery.js
+++ B1_eval_jquery_files/assets/B1_eval_jquery.js
@@ -1,5 +1,33 @@
-/* --------------------------------------------------------------------------
+function experiment(){
+    $(`#toggleRedSquare`).on(`click`, function(){
 
-Votre code ici
+        // Pour activer et désactiver le bouton
+        $(`.redsquare-app`).toggleClass(`is-active`)
 
--------------------------------------------------------------------------- */
+        // // Pour ajouter et supprimer l'attribut disabled
+        $(`.redsquare-controls`).attr(`.disabled`).removeAttr(`disabled`)
+
+        // Permuter les deux classes
+        $(`.btn-light`).toggleClass(`.btn-primary`)
+
+    })
+}
+
+function expand(){
+        // $(`#scaleUp`).on(`click`, function(){
+
+    //     if (){
+    //         $(`.)
+    //     }
+    // })
+}
+
+$(function(){
+
+    experiment()
+
+
+
+
+
+})
